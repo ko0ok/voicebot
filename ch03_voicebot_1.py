bcacb99abc3e22ab76d2f3f6e8ff25158a86c270
ko0ok <162411451+ko0ok@users.noreply.github.com>
2024-07-30 14:16:14 +0900

    Update ch03_voicebot_1.py

--- ch03_voicebot_1.py
+++ ch03_voicebot_1.py
@@ -19,7 +19,7 @@ from pydub import AudioSegment
 # 입력된 소리를 텍스트로 바꿈
 def STT(audio):
     # 파일 저장
-    filename='input.mp3'
+    filename = 'input.mp3'
     audio.export(filename, format="mp3")
     # 음원 파일 열기
     audio_file = open(filename, "rb")
@@ -40,10 +40,10 @@ def ask_gpt(prompt, model):
 def TTS(response):
     # gTTS 를 활용하여 음성 파일 생성
     filename = "output.mp3"
-    tts = gTTS(text=response,lang="ko")
+    tts = gTTS(text=response, lang="ko")
     tts.save(filename)
 
-    # 음원 파일 자동 재생생
+    # 음원 파일 자동 재생
     with open(filename, "rb") as f:
         data = f.read()
         b64 = base64.b64encode(data).decode()
@@ -52,7 +52,7 @@ def TTS(response):
             <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
             </audio>
             """
-        st.markdown(md,unsafe_allow_html=True,)
+        st.markdown(md, unsafe_allow_html=True,)
     # 파일 삭제
     os.remove(filename)
 
@@ -61,7 +61,8 @@ def main():
     # 기본 설정, 스트림릿을 활용하여 웹페이지 제작
     st.set_page_config(
         page_title="음성 비서 프로그램",
-        layout="wide")
+        layout="wide"
+    )
 
     # session state 초기화, 기존 저장된 정보가 없을 경우 사용자가 버튼을 누를 때마다 초기화되도록 함
     if "chat" not in st.session_state:
@@ -100,7 +101,7 @@ def main():
         st.markdown("---")
 
         # GPT 모델을 선택하기 위한 라디오 버튼 생성
-        model = st.radio(label="GPT 모델",options=["gpt-4", "gpt-3.5-turbo"])
+        model = st.radio(label="GPT 모델", options=["gpt-4", "gpt-3.5-turbo"])
 
         st.markdown("---")
 
@@ -110,15 +111,15 @@ def main():
             st.session_state["chat"] = []
             st.session_state["messages"] = [{"role": "system", "content": "You are a thoughtful assistant. Respond to all input in 25 words and answer in korea"}]
             st.session_state["check_reset"] = True
-            
+
     # 기능 구현 공간
-    col1, col2 =  st.columns(2)
+    col1, col2 = st.columns(2)
     with col1:
         # 왼쪽 영역 작성
         st.subheader("질문하기")
         # 음성 녹음 아이콘 추가
         audio = audiorecorder("클릭하여 녹음하기", "녹음중...")
-        if (audio.duration_seconds > 0) and (st.session_state["check_reset"]==False):
+        if (audio.duration_seconds > 0) and (st.session_state["check_reset"] == False):
             # 음성 재생 
             st.audio(audio.export().read())
             # 음원 파일에서 텍스트 추출
@@ -126,23 +127,23 @@ def main():
 
             # 채팅을 시각화하기 위해 질문 내용 저장
             now = datetime.now().strftime("%H:%M")
-            st.session_state["chat"] = st.session_state["chat"]+ [("user",now, question)]
+            st.session_state["chat"] = st.session_state["chat"] + [("user", now, question)]
             # GPT 모델에 넣을 프롬프트를 위해 질문 내용 저장
-            st.session_state["messages"] = st.session_state["messages"]+ [{"role": "user", "content": question}]
+            st.session_state["messages"] = st.session_state["messages"] + [{"role": "user", "content": question}]
 
     with col2:
         # 오른쪽 영역 작성
         st.subheader("질문/답변")
-        if  (audio.duration_seconds > 0)  and (st.session_state["check_reset"]==False):
+        if (audio.duration_seconds > 0) and (st.session_state["check_reset"] == False):
             # ChatGPT에게 답변 얻기
             response = ask_gpt(st.session_state["messages"], model)
 
             # GPT 모델에 넣을 프롬프트를 위해 답변 내용 저장
-            st.session_state["messages"] = st.session_state["messages"]+ [{"role": "system", "content": response}]
+            st.session_state["messages"] = st.session_state["messages"] + [{"role": "system", "content": response}]
 
             # 채팅 시각화를 위한 답변 내용 저장
             now = datetime.now().strftime("%H:%M")
-            st.session_state["chat"] = st.session_state["chat"]+ [("bot",now, response)]
+            st.session_state["chat"] = st.session_state["chat"] + [("bot", now, response)]
 
             # 채팅 형식으로 시각화 하기
             for sender, time, message in st.session_state["chat"]:
@@ -152,11 +153,11 @@ def main():
                 else:
                     st.write(f'<div style="display:flex;align-items:center;justify-content:flex-end;"><div style="background-color:lightgray;border-radius:12px;padding:8px 12px;margin-left:8px;">{message}</div><div style="font-size:0.8rem;color:gray;">{time}</div></div>', unsafe_allow_html=True)
                     st.write("")
-            
+
             # gTTS 를 활용하여 음성 파일 생성 및 재생
             TTS(response)
         else:
             st.session_state["check_reset"] = False
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
